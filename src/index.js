56f1e9609a9959081a55c033cec7d5c4a6f3899d
Kinyua <alfkinyua@gmail.com>
2022-07-06 19:32:29 +0300

    Add reviewers comments

--- src/index.js
+++ src/index.js
@@ -4,24 +4,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const todoItems = [
     {
       description: 'Do homework',
-      completed: 'true',
+      completed: true,
       index: 0,
     },
     {
       description: 'Go shopping',
-      completed: 'true',
+      completed: true,
       index: 1,
     },
     {
       description: 'Prepare a budget',
-      completed: 'true',
+      completed: true,
       index: 2,
     }];
   todoItems.forEach((todoItems) => {
     const display = document.querySelector('.display');
-    const items = document.createElement('li');
-    const textItems = document.createTextNode(todoItems.description);
-    items.appendChild(textItems);
+    const items = document.createElement('div');
+    items.innerHTML = `
+    <form action="/action_page.php">
+  
+  <div class ="push">
+  <input type="checkbox" id="stuff" name="stuff">
+  <label for="inputs" class="inputs"> ${todoItems.description}</label>  <i class="fa-solid fa-ellipsis-vertical"></i>  
+  </div>
+  </form>`;
+
     display.appendChild(items);
   });
 });
